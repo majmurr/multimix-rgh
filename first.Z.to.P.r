5ce5188cb440a22c64759b5b63e3552e05e40cc0
Murray Jorgensen <majmurr@gmail.com>
2020-01-19 12:51:43 +1300

    Some further changes to first.Z.to.P.r

--- first.Z.to.P.r
+++ first.Z.to.P.r
@@ -40,74 +40,71 @@ first.Z.to.P <- function(D) {
     #ovar <- ostat2 - ostat^2
     ovar <- ostat
     for (j in seq_len(q)) {
-      ovar[j,] <- varw(ovals, W[,j])
+      ovar[j,] <- diag(varw(ovals, W[,j]))
     }
     # ns
     pistat <- ppi
     for (i in seq_along(cdep) ) {
       cstat[[i]] <- crossprod(W, cvals[[i]])
-      cstat2[[i]] <- crossprod(W, cvals2[[i]])
-      cpstat[[i]] <- crossprod(W, cprods[[i]])
+      # cstat2[[i]] <- crossprod(W, cvals2[[i]])
+      # cpstat[[i]] <- crossprod(W, cprods[[i]])
     }
     for (i in seq_along(lcdep)) {
-      for (j in seq_len(ldlevs[i])){
-        group <- ldvals[[i]][,j]==1
+      for (lv in seq_len(ldlevs[i])){
+        group <- ldvals[[i]][,lv]==1
         gtot <- colSums(W[group,])   
         ### or maybe pmin(colsums(W[group,]), minpstar)
         WW <- W[group,]%*%diag(1/gtot)
-        lcstat[[i]][[j]] <- crossprod(WW,lcvals[[i]][group,])
-        lcstat2[[i]][[j]] <- crossprod(WW,lcvals2[[i]][group,])
-        lcpstat[[i]][[j]] <- crossprod(WW,lcprods[[i]][group,])
+        lcstat[[i]][[lv]] <- crossprod(WW,lcvals[[i]][group,])
+        #lcstat2[[i]][[lv]] <- crossprod(WW,lcvals2[[i]][group,])
+        #lcpstat[[i]][[lv]] <- crossprod(WW,lcprods[[i]][group,])
       }
     }
   
-  cvar <- cstat
-  ccov <- cpstat
-  for (i in seq_along(cdep) ) {
-    lcdi <- length(cdep[[i]])
-    nxp <- lcdi*(lcdi - 1)/2  
-    cvar[[i]] <-  cstat2[[i]] - {cstat[[i]]}^2
+#  cvar <- cstat
+#  ccov <- cpstat
+#  for (i in seq_along(cdep) ) {
+#     lcdi <- length(cdep[[i]])
+#    nxp <- lcdi*(lcdi - 1)/2  
+#    cvar[[i]] <-  cstat2[[i]] - {cstat[[i]]}^2
+#    for (j in 1:q) {
+#      for (k in  seq_along(cdep[[i]])) {
+#        MVMV[[i]][[j]][k, k] = cvar[[i]][j,k]
+#      }
+#    }	
+#    for (ii in seq_len(nxp)) {
+#      ccov[[i]][ ,ii] <- (cpstat[[i]][ ,ii] - 
+#                            cstat[[i]][ ,left(ii)]*cstat[[i]][ ,right(ii)])
+#    }
+#    for (j in 1:q) {
+#      for (ii in seq_len(nxp)) {
+#        MVMV[[i]][[j]][left(ii), right(ii)] <-
+#          MVMV[[i]][[j]][right(ii), left(ii)] <- ccov[[i]][j, ii]
+#      }
+#    }
+#  }
+#            
     for (j in 1:q) {
       for (k in  seq_along(cdep[[i]])) {
-        MVMV[[i]][[j]][k, k] = cvar[[i]][j,k]
+        MVMV[[i]][[j]] <- varw(cvals[[i]], W[,j])
       }
     }	
-    for (ii in seq_len(nxp)) {
-      ccov[[i]][ ,ii] <- (cpstat[[i]][ ,ii] - 
-                            cstat[[i]][ ,left(ii)]*cstat[[i]][ ,right(ii)])
-    }
-    for (j in 1:q) {
-      for (ii in seq_len(nxp)) {
-        MVMV[[i]][[j]][left(ii), right(ii)] <-
-          MVMV[[i]][[j]][right(ii), left(ii)] <- ccov[[i]][j, ii]
-      }
-    }
-  }
-  #            
+# ns    
+    
   lcvar <- lcstat
   lccov <- lcpstat
   for (i in seq_along(lcdep) ) {
     lcdi <- length(lcdep[[i]]) - 1
-    nxp <- lcdi*(lcdi - 1)/2
     for (j in 1:q) {
-      Temp <- rep(0, lcdi)
+      Temp <- 0*diag(lcdi)
       for (lv in seq_len(ldlevs[i])) {
-        lcvar[[i]][[lv]] <- lcstat2[[i]][[lv]] - {lcstat[[i]][[lv]]}^2
-        for (k in  seq_len(lcdi)) {
-          Temp[k] <- Temp[k]    +    lcvar[[i]][[lv]][j,k]*ldstat[[i]][j, lv]
-        } #k
+        group <- ldvals[[i]][,lv]==1
+        gtot <- colSums(W[group,])   
+        ### or maybe pmin(colsums(W[group,]), minpstar)
+        WW <- W[group,]%*%diag(1/gtot)
+        Temp <- Temp + varw(lcvals[[i]][group,], WW[,lv])*ldstat[j,lv]
       } #lv  
-      diag(LMV[[i]][[j]]) <- Temp 
-      M <- diag(Temp, nrow = length(Temp))
-      for (lv in seq_len(ldlevs[i])) {
-        for (ii in seq_len(nxp)) { 
-          lccov[[i]][[lv]][ ,ii] <- (lcpstat[[i]][[lv]][ ,ii] - 
-                                       lcstat[[i]][[lv]][ ,left(ii)]*lcstat[[i]][[lv]][ ,right(ii)])
-          M[right(ii), left(ii)] <- M[right(ii), left(ii)] + lccov[[i]][[lv]][j,ii]*ldstat[[i]][j, lv]
-          M[left(ii), right(ii)] <- M[right(ii), left(ii)]		
-        } #ii
-      } #lv
-      LMV[[i]][[j]] <- M
+      LMV[[i]][[j]] <- Temp
     } #j 
   } #i
   P <- list(dstat = dstat,
